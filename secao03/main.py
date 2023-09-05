5859357446b945eedf422133a97bebbb13abfb9c
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-09-05 10:11:19 -0300

    injecao de dependencias

--- secao03/main.py
+++ secao03/main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI, status, HTTPException, Response, Path, Query, Header
-from typing import Optional
+from fastapi import FastAPI, status, HTTPException, Response, Path, Query, Header, Depends
+from typing import Optional, Any
 from models import Curso
+from time import sleep
+
+def fake_db():
+    try: 
+        print('Abrindo conexão com Banco de dados...')
+        sleep(0.5)
+    finally:
+        print('Fechando conexão com Banco de dados...')
+        sleep(0.5)
 
 app = FastAPI()
 
@@ -18,11 +27,11 @@ cursos = {
 }
 
 @app.get('/cursos')
-async def get_cursos():
+async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id :int = Path(title='ID do curso', description=f'Deve estar entre 1 e 3', gt=0, lt=4)):
+async def get_curso(curso_id :int = Path(title='ID do curso', description=f'Deve estar entre 1 e 3', gt=0, lt=4), db: Any = Depends(fake_db)):
     try:
         curso = cursos[curso_id]
         curso.update({"id": curso_id})
@@ -31,14 +40,14 @@ async def get_curso(curso_id :int = Path(title='ID do curso', description=f'Deve
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
     
 @app.post('/cursos', status_code=status.HTTP_201_CREATED)
-async def post_curso(curso: Curso):
+async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
     next_id: int = len(cursos) + 1
     cursos[next_id] = curso
     del curso.id
     return curso
     
 @app.put('/cursos/{curso_id}')
-async def put_curso(curso_id: int, curso: Curso):
+async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         cursos[curso_id] = curso
         curso.id = curso_id
@@ -47,7 +56,7 @@ async def put_curso(curso_id: int, curso: Curso):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com o id {curso_id}.')
     
 @app.delete('/cursos/{curso_id}')
-async def delete_curso(curso_id: int):
+async def delete_curso(curso_id: int, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         del cursos[curso_id]
         # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
