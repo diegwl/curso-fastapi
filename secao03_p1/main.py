617136cb6c6cbfce3e6d1e875a51d168573b39ed
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-09-05 11:24:03 -0300

    validacao

--- secao03_p1/main.py
+++ secao03_p1/main.py
@@ -13,6 +13,7 @@ def fake_db():
 
 app = FastAPI(title="API de cursos da Geek University", version='0.0.1', description="Uma API para estudo do FastAPI")
 
+# GET
 @app.get('/cursos', 
         description='Retorna todos os cursos ou uma lista vazia.', 
         summary='Retorna todos os cursos', response_model=List[Curso], 
@@ -22,40 +23,48 @@ async def get_cursos(db: Any = Depends(fake_db)):
 
 @app.get('/cursos/{curso_id}')
 async def get_curso(curso_id :int = Path(title='ID do curso', 
-                                        description=f'Deve estar entre 1 e 3', gt=0, lt=4), 
+                                        description=f'Deve estar entre 1 e 3', gt=-1, lt=4), 
                                         db: Any = Depends(fake_db)):
     try:
-        curso = cursos[curso_id]
-        curso.update({"id": curso_id})
+        curso = cursos[curso_id-1]
+        curso.id = curso_id
         return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
     
+# POST
 @app.post('/cursos', status_code=status.HTTP_201_CREATED, response_model=Curso)
 async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
     next_id: int = len(cursos) + 1
-    cursos[next_id] = curso
-    del curso.id
+    curso.id = next_id
+    cursos.append(curso)
     return curso
     
+# PUT
 @app.put('/cursos/{curso_id}')
 async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
-    if curso_id in cursos:
-        cursos[curso_id] = curso
+    if curso_id <= len(cursos):
         curso.id = curso_id
+        cursos[curso_id] = curso
         return curso
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com o id {curso_id}.')
-    
+
+# DELETE 
 @app.delete('/cursos/{curso_id}')
 async def delete_curso(curso_id: int, db: Any = Depends(fake_db)):
-    if curso_id in cursos:
+    if curso_id <= len(cursos):
         del cursos[curso_id]
         # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+        cont = 0
+        for curso in cursos:
+            curso.id = cont
+            cont += 1
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com o id {curso_id}')
-    
+
+# GET com query params 
 @app.get('/calculadora')
 async def calcular(a: int = Query(gt=0), b: int = Query(lt=200), c: Optional[int] = Query(default=0), x_geek: str = Header(default=None)):
     soma = a + b + c
--- secao03_p1/models.py
+++ secao03_p1/models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Curso(BaseModel):
     id: Optional[int] = None
@@ -7,7 +7,18 @@ class Curso(BaseModel):
     aulas: int
     horas: int
 
+    @validator('titulo')
+    def validar_titulo(cls, value):
+        palavras = value.split(' ')
+        if len(palavras) < 2:
+            raise ValueError('O título deve ter pelo menos 2 palavras.')
+
+        if value.islower():
+            raise ValueError('O título precisa ser capitalizado.')
+
+        return value
+
 cursos = [
-    Curso(id=1, titulo='Programação para Leigos', aulas=42, horas=56),
-    Curso(id=2, titulo='Algoritmos e Lógica de Programação', aulas=52, horas=66),
+    Curso(id=0, titulo='Programação para Leigos', aulas=42, horas=56),
+    Curso(id=1, titulo='Algoritmos e Lógica de Programação', aulas=52, horas=66),
 ]
