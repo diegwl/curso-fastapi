0a019df8a58457848e4b482f5d318c54445bb55d
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-09-06 11:31:48 -0300

    secao04

---
+++ secao04/models/curso_model.py
@@ -0,0 +1,12 @@
+from core.configs import settings
+
+from sqlalchemy import Column, Integer, String
+
+
+class CursoModel(settings.DBBaseModel):
+    __tablename__ = 'cursos'
+
+    id: Column(Integer, primary_key=True, autoincrement=True)
+    titulo: Column(String(100))
+    aulas: int = Column(Integer)
+    horas: int = Column(Integer)
