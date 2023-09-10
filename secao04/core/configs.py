226b066a1b0b4442e29bb767ba86cc7a7e396492
Diegwl <diegocastanlopes0210@outlook.com>
2023-09-10 17:48:14 -0300

    db com bug

--- secao04/core/configs.py
+++ secao04/core/configs.py
@@ -1,16 +1,14 @@
-from typing import List
-
-from pydantic import AnyHttpUrl
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
+from typing import ClassVar
 
 class Settings(BaseSettings):
     """
     Configurações gerais usadas na aplicação
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = "postgresql+asyncpg://root:root@localhost:5432/faculdade"
-    DBBaseModel = declarative_base()
+    DB_URL: str = "postgresql+asyncpg://diego:diego@localhost:5432/faculdade"
+    DBBaseModel: ClassVar = declarative_base()
 
     class Config:
         case_sensitive = True
--- secao04/models/curso_model.py
+++ secao04/models/curso_model.py
@@ -1,12 +1,14 @@
 from core.configs import settings
 
+from sqlalchemy.orm import Mapped
+
 from sqlalchemy import Column, Integer, String
 
 
 class CursoModel(settings.DBBaseModel):
     __tablename__ = 'cursos'
 
-    id: Column(Integer, primary_key=True, autoincrement=True)
-    titulo: Column(String(100))
-    aulas: int = Column(Integer)
-    horas: int = Column(Integer)
+    id: Mapped[int] = Column(Integer, primary_key=True, autoincrement=True)
+    titulo: Mapped[str] = Column(String(100))
+    aulas: Mapped[int] = Column(Integer)
+    horas: Mapped[int] = Column(Integer)
